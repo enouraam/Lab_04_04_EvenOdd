14db0c489c17efb0600216b5f26dd1d372f5c5c2
enouraam <enouraam@mail.uc.edu>
2024-09-27 14:34:44 -0400

    commit/complete

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 2;
+        int checkEvenOdd = num % 2 ;
+        System.out.println(num + " modulo 2 is "+checkEvenOdd + " that means its even" );
+    }
+}
